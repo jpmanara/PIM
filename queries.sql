12029575836ad57097bafad8f9e7da02a173687e
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-05-04 10:49:02 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,12 @@
 /* OBTER ADICIONAIS FIXOS DO FUNCIONARIO COM ID = 1 */
-select * from Pagamento pgm
-left join Adicional_Funcionario adcfn
-on adcfn.funcionario_id = 1
-left join Adicional adc
-on adcfn.id = adc.id;
+select TipoAdicional.valor as tipo, Adicional.valor from Adicional
+left join Adicional_Funcionario on Adicional_Funcionario.id = Adicional.id
+left join TipoAdicional on TipoAdicional.cod = Adicional.tipo_adicional_cod
+where Adicional_Funcionario.funcionario_id = 1
 
 
 /* OBTER DESCONTOS FIXOS DO FUNCIONARIO COM ID = 1 */
-
+select TipoDesconto.valor as tipo, Desconto.valor from Desconto
+left join Desconto_Funcionario on Desconto_Funcionario.id = Desconto.id
+left join TipoDesconto on TipoDesconto.cod = Desconto.tipo_desconto_cod
+where Desconto_Funcionario.funcionario_id = 1

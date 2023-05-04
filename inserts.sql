325217a701191dcc4fd9cd7001c9885d5f050e6f
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-05-04 11:40:50 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -1,10 +1,15 @@
-insert into TipoPagamento (valor) values ('Mensal');
-insert into TipoDesconto (valor) values ('INSS');
-insert into TipoAdicional (valor) values ('Auxílio de custo teletrabalho');
+insert into TipoPagamento (cod, valor) values (1, 'Mensal');
+insert into TipoDesconto (cod, valor) values (1, 'INSS');
+insert into TipoAdicional (cod, valor) values (1, 'Auxílio de custo teletrabalho');
 
-insert into Funcionario (salario_base) values (3000);
-insert into Desconto (pagamento_id, tipo_desconto_cod, valor) values (1, 1, 253.64);
-insert into Adicional(pagamento_id, tipo_adicional_cod, valor) values (1, 1, 150.54);
+insert into Funcionario (id, salario_base) values (1, 3000);
+insert into Desconto (id, tipo_desconto_cod, valor) values ( 1, 1, 253.64);
+insert into Adicional(id, tipo_adicional_cod, valor) values ( 1, 1, 150.54);
 
-insert into Adicional_Funcionario (funcionario_id, adicional_id) values (1, 1);
-insert into Pagamento (funcionario_id, tipo_pagamento_cod, data_pagamento, valor) values (1, 1, '2023-05-05', 3000);
+insert into Adicional_Funcionario (id, funcionario_id, adicional_id) values (1, 1, 1);
+insert into Desconto_Funcionario (id, funcionario_id, desconto_id) values (1, 1, 1);
+
+insert into Adicional_Pagamento (id, pagamento_id, adicional_id) values (1, 1, 1);
+insert into Desconto_Pagamento (id, pagamento_id, desconto_id) values (1, 1, 1);
+
+insert into Pagamento (id, funcionario_id, tipo_pagamento_cod, data_pagamento, valor) values (1, 1, 1, '2023-05-05', 3000);

1416e64ce20e7d56f1340c09f56ec2c26b2ab8dc
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-05-04 11:49:30 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,3 +10,10 @@ select TipoDesconto.valor as tipo, Desconto.valor from Desconto
 left join Desconto_Funcionario on Desconto_Funcionario.id = Desconto.id
 left join TipoDesconto on TipoDesconto.cod = Desconto.tipo_desconto_cod
 where Desconto_Funcionario.funcionario_id = 1
+
+/* OBTER PAGAMENTOS DO FUNCIONARIO COM ID = 1 ORDENADOS POR DATA */
+select Pagamento.tipo_pagamento_cod as tipo, Pagamento.data_pagamento as data, Pagamento.valor 
+from Pagamento
+inner join TipoPagamento on TipoPagamento.cod = Pagamento.tipo_pagamento_cod
+where Pagamento.funcionario_id = 1
+order BY Pagamento.data_pagamento;

26a88d9455085e7efe9dea87a99770d015cebe00
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-05-11 20:23:09 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -25,3 +25,23 @@ select
 from Adicional
 inner join TipoAdicional on Adicional.tipo_adicional_cod = TipoAdicional.cod
 inner join Adicional_funcionario on Adicional_funcionario.funcionario_id  = 1;
+
+select 
+	 TipoDesconto.valor as tipo_desconto, 
+	 Desconto.valor_fixo,
+	 Desconto.porcentagem, 
+	 Desconto.min_salario, 
+	 Desconto.max_salario 
+from Desconto
+inner join TipoDesconto on Desconto.tipo_desconto_cod = TipoDesconto.cod
+inner join Desconto_pagamento on Desconto_pagamento.pagamento_id  = 3;
+
+select 
+	 TipoAdicional.valor as tipo_adicional, 
+	 Adicional.valor_fixo,
+	 Adicional.porcentagem, 
+	 Adicional.min_salario, 
+	 Adicional.max_salario 
+from Adicional
+inner join TipoAdicional on Adicional.tipo_adicional_cod = TipoAdicional.cod
+inner join Adicional_Pagamento on Adicional_Pagamento.pagamento_id  = 3;

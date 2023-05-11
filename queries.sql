19ee014fbaa92a3b54394521f2244f00b219deac
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-05-11 20:14:37 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,19 +1,27 @@
-/* OBTER ADICIONAIS FIXOS DO FUNCIONARIO COM ID = 1 */
-select TipoAdicional.valor as tipo, Adicional.valor from Adicional
-left join Adicional_Funcionario on Adicional_Funcionario.id = Adicional.id
-left join TipoAdicional on TipoAdicional.cod = Adicional.tipo_adicional_cod
-where Adicional_Funcionario.funcionario_id = 1
+select 
+	Funcionario.nome, 
+	TipoCargo.valor as cargo, 
+	Funcionario.salario_base ,
+	Funcionario.jornada_trabalho_semanal 
+from Funcionario
+inner join TipoCargo on Funcionario.tipo_cargo_cod = TipoCargo.cod
 
+select 
+	 TipoDesconto.valor as tipo_desconto, 
+	 Desconto.valor_fixo,
+	 Desconto.porcentagem, 
+	 Desconto.min_salario, 
+	 Desconto.max_salario 
+from Desconto
+inner join TipoDesconto on Desconto.tipo_desconto_cod = TipoDesconto.cod
+inner join Desconto_funcionario on Desconto_funcionario.funcionario_id  = 1;
 
-/* OBTER DESCONTOS FIXOS DO FUNCIONARIO COM ID = 1 */
-select TipoDesconto.valor as tipo, Desconto.valor from Desconto
-left join Desconto_Funcionario on Desconto_Funcionario.id = Desconto.id
-left join TipoDesconto on TipoDesconto.cod = Desconto.tipo_desconto_cod
-where Desconto_Funcionario.funcionario_id = 1
-
-/* OBTER PAGAMENTOS DO FUNCIONARIO COM ID = 1 ORDENADOS POR DATA */
-select TipoPagamento.valor as tipo, Pagamento.data_pagamento as data, Pagamento.valor
-from Pagamento
-inner join TipoPagamento on TipoPagamento.cod = Pagamento.tipo_pagamento_cod
-where Pagamento.funcionario_id = 1
-order BY Pagamento.data_pagamento;
+select 
+	 TipoAdicional.valor as tipo_adicional, 
+	 Adicional.valor_fixo,
+	 Adicional.porcentagem, 
+	 Adicional.min_salario, 
+	 Adicional.max_salario 
+from Adicional
+inner join TipoAdicional on Adicional.tipo_adicional_cod = TipoAdicional.cod
+inner join Adicional_funcionario on Adicional_funcionario.funcionario_id  = 1;

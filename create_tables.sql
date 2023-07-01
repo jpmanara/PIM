7693b8e3ce3efa9a9ad57a850346431e29dabb2f
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-06-30 21:31:56 -0300

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -4,20 +4,21 @@ create table TipoAdicional (cod int identity(1,1) not null, valor varchar(255), 
 create table TipoCargo (cod int identity(1,1) not null, valor varchar(255), primary key (cod));
 create Table TipoUsuario (cod int identity(1,1) not null, valor varchar(255), primary key (cod));
 
-create table Empresa {
+create table Empresa (
    id int identity(1,1) not null,
    cpf_cnpj varchar(14) not null,
    primary key(id)
-}
+)
 
-create table Usuario {
+create table Usuario (
   id int identity(1,1) not null,
   tipo_usuario_cod int not null,
   email varchar(255) not null unique,
   senha varchar(255) not null,
   foreign key (tipo_usuario_cod) references TipoUsuario (cod),
   primary key(email)
-}
+)
+
 
 create table Funcionario (
 	id int identity(1,1) not null,
@@ -27,7 +28,7 @@ create table Funcionario (
 	tipo_cargo_cod int not null, 
 	salario_base float not null,
 	jornada_trabalho_semanal float not null,
-	usuario_id int not null,
+	usuario_id varchar(255) not null,
 	empresa_id int not null,
 	
 	foreign key (empresa_id) references Empresa (id),
@@ -107,4 +108,6 @@ create table Desconto_Pagamento (
 	foreign key (pagamento_id) references Pagamento(id),
 	foreign key (desconto_id) references Desconto(id),
 	primary key (id)
+
+
 );

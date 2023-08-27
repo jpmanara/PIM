8f8a309c8d8a25bdc5eee06b06b37b4b3ec9fda9
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-08-27 00:19:37 -0300

    Update create_tables.sql, trocando convencao nomenclatura

--- create_tables.sql
+++ create_tables.sql
@@ -7,107 +7,107 @@ create Table TipoUsuario (cod int identity(1,1) not null, valor varchar(255), pr
 create table Empresa (
    id int identity(1,1) not null,
    nome varchar(255) not null,
-   cpf_cnpj varchar(14) not null unique,
+   cpfCnpj varchar(14) not null unique,
    primary key(id)
 )
 
 create table Usuario (
   id int identity(1,1) not null,
-  tipo_usuario_cod int not null,
+  tipoUsuarioCod int not null,
   email varchar(255) not null unique,
   senha varchar(255) not null,
-  foreign key (tipo_usuario_cod) references TipoUsuario (cod),
+  foreign key (tipoUsuarioCod) references TipoUsuario (cod),
   primary key(id)
 )
 
 
 create table Funcionario (
 	id int identity(1,1) not null,
-	nome varchar(255) not null,
-	sobrenome varchar(255) not null,
+	nomeCompleto varchar(255) not null,
 	cpf varchar(11) not null unique,
-	tipo_cargo_cod int not null, 
-	salario_base float not null,
-	jornada_trabalho_semanal float not null,
-	usuario_id int not null,
-	empresa_id int not null,
+	endereco varchar(255) not null,
+	tipoCargoCod int not null, 
+	salarioBase float not null,
+	jornadaTrabalhoSemanal float not null,
+	usuarioId int not null,
+	empresaId int not null,
 	
-	foreign key (empresa_id) references Empresa (id),
-	foreign key (usuario_id) references Usuario (id),
-	foreign key (tipo_cargo_cod) references TipoCargo (cod),
+	foreign key (empresaId) references Empresa (id),
+	foreign key (usuarioId) references Usuario (id),
+	foreign key (tipoCargoCod) references TipoCargo (cod),
 	primary key (id)
 );
 
 create table Pagamento (
 	id int identity(1,1) not null, 
-	funcionario_id int not null, 
-	tipo_pagamento_cod int not null,
-	data_pagamento DATE not null,
-	horas_trabalhadas float not null,
+	funcionarioId int not null, 
+	tipoPagamentoCod int not null,
+	dataPagamento DATE not null,
+	horasTrabalhadas float not null,
 	valor float not null,
 
 	primary key (id),
-	foreign key (tipo_pagamento_cod) references TipoPagamento (cod),
-	foreign key (funcionario_id) references Funcionario(id)
+	foreign key (tipoPagamentoCod) references TipoPagamento (cod),
+	foreign key (funcionarioId) references Funcionario(id)
 );
 
 create table Desconto (
 	id int identity(1,1) not null,
-	tipo_desconto_cod int not null,
-	valor_fixo float,
+	tipoDescontoCod int not null,
+	valorFixo float,
 	porcentagem int,
-	min_salario int,
-	max_salario int,
+	minSalario int,
+	maxSalario int,
 
 	primary key(id),
-	foreign key (tipo_desconto_cod) references TipoDesconto (cod)
+	foreign key (tipoDescontoCod) references TipoDesconto (cod)
 );
 
 create table Adicional (
 	id int identity(1,1) not null,
-	tipo_adicional_cod int not null,
-	valor_fixo float not null,
+	tipoAdicionalCod int not null,
+	valorFixo float not null,
 	porcentagem int,
-	min_salario int,
-	max_salario int,
+	minSalario int,
+	maxSalario int,
 
 	primary key(id),
-	foreign key (tipo_adicional_cod) references TipoAdicional (cod)
+	foreign key (tipoAdicionalCod) references TipoAdicional (cod)
 );
 
-create table Adicional_Funcionario (
+create table AdicionalFuncionario (
 	id int identity(1,1) not null,
-	funcionario_id int not null,
-	adicional_id int not null,
-	foreign key (funcionario_id) references Funcionario(id),
-	foreign key (adicional_id) references Adicional(id),
+	funcionarioId int not null,
+	adicionalId int not null,
+	foreign key (funcionarioId) references Funcionario(id),
+	foreign key (adicionalId) references Adicional(id),
 	primary key (id)
 );
 
-create table Desconto_Funcionario (
+create table DescontoFuncionario (
 	id int identity(1,1) not null,
-	funcionario_id int not null,
-	desconto_id int not null,
-	foreign key (funcionario_id) references Funcionario(id),
-	foreign key (desconto_id) references Desconto(id),
+	funcionarioId int not null,
+	descontoId int not null,
+	foreign key (funcionarioId) references Funcionario(id),
+	foreign key (descontoId) references Desconto(id),
 	primary key (id)
 );
 
-create table Adicional_Pagamento (
+create table AdicionalPagamento (
 	id int identity(1,1) not null,
-	pagamento_id int not null,
-	adicional_id int not null,
-	foreign key (pagamento_id) references Pagamento(id),
-	foreign key (adicional_id) references Adicional(id),
+	pagamentoId int not null,
+	adicionalId int not null,
+	foreign key (pagamentoId) references Pagamento(id),
+	foreign key (adicionalId) references Adicional(id),
 	primary key (id)
 );
 
-create table Desconto_Pagamento (
+create table DescontoPagamento (
 	id int identity(1,1) not null,
-	pagamento_id int not null,
-	desconto_id int not null,
-	foreign key (pagamento_id) references Pagamento(id),
-	foreign key (desconto_id) references Desconto(id),
+	pagamentoId int not null,
+	descontoId int not null,
+	foreign key (pagamentoId) references Pagamento(id),
+	foreign key (descontoId) references Desconto(id),
 	primary key (id)
 
 

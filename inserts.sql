1d49af1dd4f4ac9542f2831edf9cea86f7ba327d
João Pedro Gonçalves Manara <53958777+JpythonS@users.noreply.github.com>
2023-10-13 02:40:23 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -22,9 +22,9 @@ insert into Empresas (nome, cnpj) values ('CI&T software', '01236547898745');
 insert into Usuarios (tipoUsuarioCod, email, senha) values (1, 'joao@gmail.com', '12Qwaszx!');
 
 insert into Funcionarios
-(nomeCompleto, dataNascimento, cpf, rg, celular, celularContatoEmergencia, endereco, bairro, cidade, estado, pis, tipoCargoCod, salarioBase, jornadaTrabalhoSemanal, empresaId) 
+(nomeCompleto, dataNascimento, cpf, rg, celular, celularContatoEmergencia, endereco, bairro, cidade, estado, pis, tipoCargoCod, salarioBase, jornadaTrabalhoSemanal, usuarioId, empresaId) 
 values 
-('João Guilherme Rodrigues','2023-09-26T10:00:00','12365478987', '477856358', '7778998784547', '7778998784547', 'Rua das Hortaliças', 'Jardim Florido', 'São Caetano', 'São Paulo', '78541296587', 1,3000,40,1);
+('João Guilherme Rodrigues','2023-09-26T10:00:00','12365478987', '477856358', '7778998784547', '7778998784547', 'Rua das Hortaliças', 'Jardim Florido', 'São Caetano', 'São Paulo', '78541296587', 1,3000,40, ''joao@gmail.com'', 1);
 insert into Descontos (tipoDescontoCod, valorFixo, porcentagem, minSalario, maxSalario) values (1, NULL, 7.5, 0, 1110);
 insert into Descontos (tipoDescontoCod, valorFixo, porcentagem, minSalario, maxSalario) values (1, NULL, 9, 1110.01, 2203.48);
 insert into Descontos (tipoDescontoCod, valorFixo, porcentagem, minSalario, maxSalario) values (1, NULL, 12, 2203.48, 3305.22);
